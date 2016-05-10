bbf504dc6f6055ff5d0de52f641afde36e97c46b
Your Name <you@example.com>
2016-05-10 17:12:46 +0900

    Chronometerをtextviewに変更

--- app/src/main/java/com/example/kouhei/stopwatch_app/MainActivity.java
+++ app/src/main/java/com/example/kouhei/stopwatch_app/MainActivity.java
@@ -8,15 +8,20 @@ import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     //変数
      Chronometer chronometer;
-     public TextView tv;
+     public TextView tv_log;
      public String s;
-
+     public TextView tv_time;
+     public Date date;
+     public SimpleDateFormat sdf;
 
 
     @Override
@@ -24,42 +29,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     //変数
-        final long[] l = {0};
+
     //インスタンスゾーン
-        chronometer = (Chronometer) findViewById(R.id.time);//メーター
+        tv_time = (TextView) findViewById(R.id.time);//メーター
 
         Button startButton = (Button)findViewById(R.id.b1);//スタートボタン
         Button stopButton = (Button) findViewById(R.id.b2);//ストップボタン
         Button wrapButton = (Button) findViewById(R.id.b3);//ラップタイム
         Button resetButton = (Button) findViewById(R.id.b4);//ラップタイム
 
-         tv = (TextView) findViewById(R.id.log);
+         tv_log = (TextView) findViewById(R.id.log);
 
         StringBuilder sb  = new StringBuilder();
 
-        //テキスト
-        //tv.setText("team ironman");
+         date = new Date();
+        sdf = new SimpleDateFormat("HH:mm:ss");
+
+    //メゾット
 
         startButton.setOnClickListener(new View.OnClickListener() {//スタート
             @Override
             public void onClick(View v) {
-                chronometer.start();
+                //start
+                set_time();
+
             }
         });
 
         stopButton.setOnClickListener(new View.OnClickListener() {//ストップ
             @Override
             public void onClick(View v) {
-                chronometer.stop();
-            }
+            //stop
+                 }
         });
 
         wrapButton.setOnClickListener(new View.OnClickListener() {//ラップタイム
             @Override
             public void onClick(View v) {
              //ラップ処理
-                s = settime(chronometer.getBase());
-                tv.setText(s);
 
 
             }
@@ -69,19 +76,23 @@ public class MainActivity extends AppCompatActivity {
         resetButton.setOnClickListener(new View.OnClickListener() {//リセット
             @Override
             public void onClick(View v) {
-                chronometer.setBase(SystemClock.elapsedRealtime());
-
+               //reset
+                reset_time();
             }
         });
 
 
     }
 
+     void reset_time() {
+         tv_time.setText("00:00:00");
 
-    //メゾピアノ
-    public String settime(long v){
-        String vv = Long.toString(v);
-        return vv;
-    }
+     }
+
+
+    public void set_time(){
+
+    tv_time.setText(sdf.format(date));
+}
 
 }
